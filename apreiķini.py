4ba5c7d344990599f3eb123b69b4258d840000cc
alex23777399-ui <alex23777399@gmail.com>
2026-03-04 14:37:04 +0200

    apreiķini veiksanass

---
+++ apreiķini.py
@@ -0,0 +1,5 @@
+Izmaksas:cementa izmaksas = cements × cementa_cena
+smilsu izmaksas = smiltis × smilsu_cena
+udens izmaksas = udens × udens_cena
+Summa massa = (Cementa daudzums+ Smilšu daudzums+ Ūdens daudzums)*0,95 
+Summa izmaksa = cementa izmaksas+ smilsu izmaksas+ smilsu izmaksas
